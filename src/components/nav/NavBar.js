84a12e2deaf6ae855a2ad5d09e629d259558b748
Billy Mathison <wlmathison@gmail.com>
2019-04-29 17:50:08 -0500

    Removing Bulma and Bloomer and using Reactstrap instead. Beginning to create navbar using reactstrap

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react"
-import { Link } from "react-router-dom"
-import "bulma/css/bulma.css"
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink, Nav } from "reactstrap"
 
 export default class NavBar extends Component {
+
+    state = {
+        isOpen: false,
+    }
+
+    toggleNav = () => {
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
+        }))
+    }
+
+    // Function to clear out session storage and local storage when a user clicks the log out button
     handleLogOut = event => {
         sessionStorage.clear()
         localStorage.clear()
     }
 
     render() {
-        return(
+        return (
             <React.Fragment>
-                <nav className="navbar" role="navigation" aria-label="main-navigation">
-                <div className="navbar-brand">
-            Hello
-                </div>
-                </nav>
-            </React.Fragment>
+                <Navbar fixed="top">
+                    <NavbarBrand>dram</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.isOpen} navbar>
+                        <Nav>
+                            <NavItem>
+                                <NavLink to="/whiskies">Whiskies</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </React.Fragment >
         )
     }
 }
