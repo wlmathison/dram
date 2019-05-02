0aa640349fbba9bd09ec9c74356f1d30e58c8a73
Billy Mathison <wlmathison@gmail.com>
2019-05-02 11:24:13 -0500

    Adding if statement to only display completed tastings

--- src/components/tastings/TastingIndividualCard.js
+++ src/components/tastings/TastingIndividualCard.js
@@ -22,32 +22,36 @@ export default class TastingIndividualCard extends Component {
     }
 
     render() {
-        return (
-            <React.Fragment>
-                <Card>
-                    <CardBody>
-                        <CardTitle>Tasting #{this.props.tasting.id}</CardTitle>
-                        <CardText>Date: {this.props.tasting.date}</CardText>
-                        <CardText>Theme: {this.props.tasting.theme}</CardText>
-                        <CardText>Whiskies:</CardText>
-                        {this.state.tastingSelections.map(tastingSelection => {
-                            if (tastingSelection.tastingId === this.props.tasting.id) {
-                                return <CardText key={tastingSelection.id}> - {tastingSelection.whiskey.name} </CardText>
-                            } else {
-                                return null
-                            }
-                        })}
-                        <CardText>Attendees: </CardText>
-                        {this.state.tastingAttendance.map(tastingAttendance => {
-                            if (tastingAttendance.tastingId === this.props.tasting.id) {
-                                return <CardText key={tastingAttendance.id}> - {tastingAttendance.user.userName} </CardText>
-                            } else {
-                                return null
-                            }
-                        })}
-                    </CardBody>
-                </Card>
-            </React.Fragment>
-        )
+        if (this.props.tasting.isComplete) {
+            return (
+                <React.Fragment>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Tasting #{this.props.tasting.id}</CardTitle>
+                            <CardText>Date: {this.props.tasting.date}</CardText>
+                            <CardText>Theme: {this.props.tasting.theme}</CardText>
+                            <CardText>Whiskies:</CardText>
+                            {this.state.tastingSelections.map(tastingSelection => {
+                                if (tastingSelection.tastingId === this.props.tasting.id) {
+                                    return <CardText key={tastingSelection.id}> - {tastingSelection.whiskey.name} </CardText>
+                                } else {
+                                    return null
+                                }
+                            })}
+                            <CardText>Attendees: </CardText>
+                            {this.state.tastingAttendance.map(tastingAttendance => {
+                                if (tastingAttendance.tastingId === this.props.tasting.id) {
+                                    return <CardText key={tastingAttendance.id}> - {tastingAttendance.user.userName} </CardText>
+                                } else {
+                                    return null
+                                }
+                            })}
+                        </CardBody>
+                    </Card>
+                </React.Fragment>
+            )
+        } else {
+            return null
+        }
     }
 }
