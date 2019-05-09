248478e0248aa1a0295e791f184a36d02aa27f1d
Billy Mathison <wlmathison@gmail.com>
2019-05-09 14:26:47 -0500

    completing styling of whiskies pages and adding background image to all empty body space of all pages

--- src/components/whiskies/WhiskeySearchForm.js
+++ src/components/whiskies/WhiskeySearchForm.js
@@ -1,33 +1,53 @@
 // Building a search form for with search option buttons to be added to WhiskeyList
 
 import React, { Component } from "react"
-import { CardBody, Form, Button, CardTitle } from "reactstrap"
+import { CardBody, Form, Button, CardTitle, Card, CardHeader } from "reactstrap"
 
 export default class WhiskeySearchForm extends Component {
     render() {
         return (
             <React.Fragment>
-                <CardTitle>Search for whiskies by: </CardTitle>
-                <CardBody>
-                    <Form>
-                        <Button
-                            type="radio"
-                            onClick={this.props.handleSearchWhiskiesByName}
-                        >Name</Button> {" "}
-                        <Button
-                            type="radio"
-                            onClick={this.props.handleSearchWhiskiesByCategory}
-                        >Category</Button> {" "}
-                        <Button
-                            type="radio"
-                            onClick={this.props.handleSearchWhiskiesByDistillery}
-                        >Distillery</Button> {" "}
-                        <Button
-                            type="radio"
-                            onClick={this.props.handleSearchAllWhiskies}
-                        >All Whiskies</Button>
-                    </Form>
-                </CardBody>
+                <Card
+                    className="card-search">
+                    <CardBody>
+                        <Card
+                            className="card-extra-opacity">
+                            <CardTitle
+                                className="search-form-title"
+                                tag={"h5"}>Search for whiskies by: </CardTitle>
+                            <CardBody>
+                                <Form
+                                    className="search-form-buttons">
+                                    <Button
+                                        className="search-buttons"
+                                        color="info"
+                                        type="radio"
+                                        onClick={this.props.handleSearchWhiskiesByName}
+                                    >Name</Button>
+                                    <Button
+                                        className="search-buttons"
+                                        color="info"
+                                        type="radio"
+                                        onClick={this.props.handleSearchWhiskiesByCategory}
+                                    >Category</Button>
+                                    <Button
+                                        className="search-buttons"
+                                        color="info"
+                                        type="radio"
+                                        onClick={this.props.handleSearchWhiskiesByDistillery}
+                                    >Distillery</Button>
+                                    <Button
+                                        className="search-buttons"
+                                        color="info"
+                                        type="radio"
+                                        onClick={this.props.handleSearchAllWhiskies}
+                                    >All Whiskies</Button>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </CardBody>
+                    <CardHeader></CardHeader>
+                </Card>
             </React.Fragment>
         )
     }
